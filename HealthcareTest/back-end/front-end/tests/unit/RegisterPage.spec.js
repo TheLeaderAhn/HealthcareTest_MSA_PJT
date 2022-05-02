6c71482757edbdc16b46688180e58196e748d988
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-05-03 08:30:46 +0900

    form 기본 형식 +  run test 완료

---
+++ HealthcareTest/back-end/front-end/tests/unit/RegisterPage.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+import RegisterPage from '@/views/RegisterPage'
+
+describe('RegisterPage.vue', () => {
+  let wrapper
+  let fieldUsername
+  let fieldEmailAddress
+  let fieldPassword
+  let buttonSubmit
+
+  beforeEach(() => {
+    wrapper = mount(RegisterPage)
+    fieldUsername = wrapper.find('#username')
+    fieldEmailAddress = wrapper.find('#emailAddress')
+    fieldPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type="submit"]')
+  })
+
+  it('should render correct contents', () => {
+    expect(wrapper.find('.logo').attributes().src).toEqual('/static/images/logo.png')
+    expect(wrapper.find('.tagline').text()).toEqual('Healcare management tool')
+    expect(fieldUsername.element.value).toEqual('')
+    expect(fieldEmailAddress.element.value).toEqual('')
+    expect(fieldPassword.element.value).toEqual('')
+    expect(buttonSubmit.text()).toEqual('Create account')
+  })
+
+  it('should contain data model with initial values', () => {
+    expect(wrapper.vm.form.username).toEqual('')
+    expect(wrapper.vm.form.emailAddress).toEqual('')
+    expect(wrapper.vm.form.password).toEqual('')
+  })
+})
